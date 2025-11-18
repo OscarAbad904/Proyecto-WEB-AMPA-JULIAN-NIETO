3016c2beadfe217b5469e8b5cf2120c501c995b3
OscarAbad904 <sinnombre904@hotmail.com>
2025-11-18 17:00:42 +0100

    Estilos Corregidos

--- manage.py
+++ manage.py
@@ -26,4 +26,4 @@ def create_admin():
     print(f"Created admin user {email}")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 3000)))
